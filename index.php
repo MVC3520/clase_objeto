0c47b452a25493325ad1f85030e877341a78c0ac
MVC3520 <81169874+MVC3520@users.noreply.github.com>
2023-11-30 15:43:36 -0300

    commit 3

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $productos = datos_productos();
 
 foreach ($productos as $producto) {
     echo '<div class="card">';
+    echo '<img src="' . $producto['imagen'] . '" alt="Imagen del producto"  style="width: 100%; height: 250px;">';
     echo '<h3>ID: ' . $producto['id'] . '</h3>';
     echo '<p>Nombre: ' . $producto['nombre'] . '</p>';
     echo '<p>Precio: $' . $producto['precio'] . '</p>';
